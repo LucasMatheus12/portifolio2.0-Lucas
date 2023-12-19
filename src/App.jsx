d3f217e16b5dc891cb20bd9041fdeb300a2db634
Lucas Matheus <matheusalexandre012@gmail.com>
2023-12-19 19:50:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './ui/css/reset.css'
+import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+   <nav>
+    <h1>olá Lucas</h1>
+   </nav>
+  )
+}
+
+export default App
